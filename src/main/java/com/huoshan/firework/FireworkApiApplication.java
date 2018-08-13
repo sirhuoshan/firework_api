2651fe9d7984732afce747e70514abc5d7b4cd5d
sirhuoshan <sirhuoshan@sirhuoshandeMacBook-Pro.local>
2018-08-13 17:12:24 +0800

    Initial commit

---
+++ src/main/java/com/huoshan/firework/FireworkApiApplication.java
@@ -0,0 +1,12 @@
+package com.huoshan.firework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireworkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireworkApiApplication.class, args);
+	}
+}
